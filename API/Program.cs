0efea371c3a5ae9e4ee39438c9f7dcefdd88724e
Khodani <khodanym@gmail.com>
2024-10-15 14:50:13 +0200

    End of session 2

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(opt=>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
