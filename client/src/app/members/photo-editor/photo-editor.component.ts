3899b55375e34e02c5ec9284fb71ab71a8756f4f
Khodani <khodanym@gmail.com>
2025-04-01 10:22:43 +0200

    End S 12

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -80,7 +80,22 @@ export class PhotoEditorComponent implements OnInit{
       const photo = JSON.parse(response);
       const updatedMember = {...this.member()}
       updatedMember.photos.push(photo);
-      this.memberChange.emit(updatedMember)
+      this.memberChange.emit(updatedMember);
+
+      if(photo.isMain){
+        const user = this.accountService.currentUser();
+        if(user){
+          user.photoUrl =photo.url;
+          this.accountService.setCuurentUser(user);
+        }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p=>{
+          if(p.isMain) p.isMain=false;
+          if(p.id==photo.id) p.isMain = true;
+        });
+
+        this.memberChange.emit(updatedMember);
+      }
 
       
     }
